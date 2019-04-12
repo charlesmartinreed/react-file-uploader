0c0d5fd1aa77ae4e07ccaf1b29455c691d5d4a65
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-12 17:12:33 -0500

    Create-React-App. Implement Bootstrap.

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import "./App.css";
+
+// using React Hooks, so we don't need a class component
+const App = () => <div className="App">app</div>;
+
+export default App;
