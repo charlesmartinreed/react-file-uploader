c779f16d5cd0fcbda865e1006088d885f0eaf2a8
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-29 15:37:51 -0500

    Wipe old project. Rewrite server.js.

--- server.js
+++ server.js
@@ -2,28 +2,25 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 
 const app = express();
+
+// MIDDLEWARE SETUP
 app.use(fileUpload());
 
-// ROUTES
+// UPLOAD ENDPOINT
 app.post("/upload", (req, res) => {
-  // check that the request if a file - if not, send a 400 bad request
-  if (req.files === null) {
-    return res.status(400).json({ msg: "No file uploaded" });
+  if (Object.keys(req.files).length === 0) {
+    res.status(400).json({ msg: "No file was uploaded" });
   }
 
-  // if req was a file...
-  const file = req.files.file;
-  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-    if (err) {
-      // send a 500 Internal Server Error
-      console.error(err);
-      return res.status(500).send(err);
-    }
-    // if upload was succesful, send the filename and the file path
-    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-  });
-});
+  let file = req.files.file;
 
-app.listen(5000, () => {
-  console.log("Server started successfully!");
+  // move the file to the appropriate place on your server
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    console.error(err);
+    return res.status(500).send(err);
+  });
+
+  res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
 });
+// SERVER UPLOADED
+app.listen(5000, () => console.log(`Server Started...`));
