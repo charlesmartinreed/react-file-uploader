256df3d30fbcbb3c452b20b41f9e4fb0839be531
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-29 15:57:06 -0500

    Implement reactstrap, react-icons.

---
+++ client/README.md
@@ -0,0 +1,6 @@
+## This project uses:
+
+- React (including Hooks for stateful functional components)
+- Express
+- express-fileuploader
+- Axios for fetching
---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { FaReact } from "react-icons/fa";
+// import { } from 'react-icons/fa'
+
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <FaReact /> React Image Uploader
+    </h4>
+    // file upload component
+  </div>
+);
+
+export default App;
